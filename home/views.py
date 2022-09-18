2cc4ad7055253c30cb043099084fdc283b14e6ce
Aayush <aceraayush@gmail.com>
2022-09-18 17:20:59 +0545

    Contact model created

--- home/views.py
+++ home/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request,'index.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'aboeut.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(name = name,email = email,subject = subject,message = message)
+        data.save()
     return render(request,'contact.html')
 
 def portfolio(request):
